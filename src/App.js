bd455fec48263f0135066e7d33f077ecb5fb1ecb
himoura32 <shawn.ahearn@outlook.com>
2022-07-26 11:32:34 -0400

    coded firestore hook/backend for addDocuments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -6,7 +6,7 @@ import Signup from './pages/signup/Signup';
 import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <div className='App'>
@@ -15,13 +15,16 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path='/'>
-              <Home />
+              {!user && <Redirect to='/login' />}
+              {user && <Home />}
             </Route>
             <Route path='/login'>
-              <Login />
+              {user && <Redirect to='/' />}
+              {!user && <Login />}
             </Route>
             <Route path='/signup'>
-              <Signup />
+              {user && <Redirect to='/' />}
+              {!user && <Signup />}
             </Route>
           </Switch>
         </BrowserRouter>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import styles from './Home.module.css';
+import TransactionForm from './TransactionForm';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export default function Home() {
-  return <div>Home</div>;
+  const { user } = useAuthContext();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm uid={user.uid} />
+      </div>
+    </div>
+  );
 }
